af7755d267bef54aba60fb64ace43cb3483840aa
aatishgyawali <aatishgyawali10@gmail.com>
2023-01-28 13:47:22 +0000

    Editing listen on port command for AWS

--- server.js
+++ server.js
@@ -112,8 +112,7 @@ app.use(function(req,res){
    res.status(404).send("Resources not found");
 });
 
-
-
-app.listen(3000,function(){
-    console.log("App started on port 3000");
+const port=process.env.PORT || 3000;
+app.listen(port,function(){
+    console.log("App started on port: "+port);
 })
